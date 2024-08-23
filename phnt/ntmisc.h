a37180332887297c0a803df692a56dc5e99e2c89
Dump-GUY <59492005+Dump-GUY@users.noreply.github.com>
2024-08-23 22:53:57 +0200

    Add files via upload

---
+++ phnt/ntmisc.h
@@ -0,0 +1,45 @@
+/*
+ * Trace Control support functions
+ *
+ * This file is part of System Informer.
+ */
+
+#ifndef _NTMISC_H
+#define _NTMISC_H
+
+// Filter manager
+
+#define FLT_PORT_CONNECT 0x0001
+#define FLT_PORT_ALL_ACCESS (FLT_PORT_CONNECT | STANDARD_RIGHTS_ALL)
+
+// VDM
+
+typedef enum _VDMSERVICECLASS
+{
+    VdmStartExecution,
+    VdmQueueInterrupt,
+    VdmDelayInterrupt,
+    VdmInitialize,
+    VdmFeatures,
+    VdmSetInt21Handler,
+    VdmQueryDir,
+    VdmPrinterDirectIoOpen,
+    VdmPrinterDirectIoClose,
+    VdmPrinterInitialize,
+    VdmSetLdtEntries,
+    VdmSetProcessLdtInfo,
+    VdmAdlibEmulation,
+    VdmPMCliControl,
+    VdmQueryVdmProcess,
+    VdmPreInitialize
+} VDMSERVICECLASS, *PVDMSERVICECLASS;
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtVdmControl(
+    _In_ VDMSERVICECLASS Service,
+    _Inout_ PVOID ServiceData
+    );
+
+#endif
---
+++ phnt_include.h
@@ -0,0 +1,3 @@
+#include <phnt_windows.h>
+#include <phnt.h>
+#define PHNT_VERSION PHNT_WIN11 // Windows 11
